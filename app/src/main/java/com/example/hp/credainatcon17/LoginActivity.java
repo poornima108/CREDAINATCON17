9f63f7e107df0d8129c6441667600069768739db
Rahul <rahulsakaria97@gmail.com>
2017-02-16 21:40:12 +0530

    Registration Screen

--- app/src/main/java/com/example/hp/credainatcon17/LoginActivity.java
+++ app/src/main/java/com/example/hp/credainatcon17/LoginActivity.java
@@ -31,7 +31,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             loginUser();
         }
         if (view == sign_up){
-            finish();
             signupUser();
         }
     }
