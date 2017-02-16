1a780618821487b0df9bd3a387ce5ccba7747600
Rahul <rahulsakaria97@gmail.com>
2017-02-16 11:37:00 +0530

    Register Activity Added

--- app/src/main/java/com/example/hp/credainatcon17/LoginActivity.java
+++ app/src/main/java/com/example/hp/credainatcon17/LoginActivity.java
@@ -1,13 +1,46 @@
 package com.example.hp.credainatcon17;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    private EditText email_id,pass_word;
+    private Button log_in;
+    private TextView sign_up;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        email_id = (EditText) findViewById(R.id.email_id_login);
+        pass_word = (EditText) findViewById(R.id.password_login);
+        sign_up = (TextView) findViewById(R.id.account_sign_up);
+        log_in = (Button) findViewById(R.id.login_button);
+        sign_up.setOnClickListener(this);
+        log_in.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == log_in){
+            loginUser();
+        }
+        if (view == sign_up){
+            finish();
+            signupUser();
+        }
+    }
+
+    private void signupUser() {
+        startActivity(new Intent(this,SignUpActivity.class));
+    }
+
+    private void loginUser() {
+        
     }
 }
--- app/src/main/java/com/example/hp/credainatcon17/MainActivity.java
+++ app/src/main/java/com/example/hp/credainatcon17/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.hp.credainatcon17;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private static int SPLASH_TIME_OUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
 }
