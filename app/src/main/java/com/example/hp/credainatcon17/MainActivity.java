056fcfc9107e24a9c860a4c0e139747f3532f819
Rahul <rahulsakaria97@gmail.com>
2017-02-17 13:49:00 +0530

    TextInputLayout added

--- app/src/main/java/com/example/hp/credainatcon17/MainActivity.java
+++ app/src/main/java/com/example/hp/credainatcon17/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.Window;
 
 public class MainActivity extends Activity {
 
-    private static int SPLASH_TIME_OUT = 4000;
+    private static int SPLASH_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
