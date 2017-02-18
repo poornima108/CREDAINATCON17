12ecc6420179ec45356173cb239a42e79d099b97
agaurang1404 <gaurang.agarwal1404@gmail.com>
2017-02-18 05:32:28 -0800

    Tab layout added

--- app/src/main/java/com/example/hp/credainatcon17/LoginActivity.java
+++ app/src/main/java/com/example/hp/credainatcon17/LoginActivity.java
@@ -40,6 +40,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     }
 
     private void loginUser() {
+        startActivity(new Intent(this,ContactUs.class));
 
     }
 }
