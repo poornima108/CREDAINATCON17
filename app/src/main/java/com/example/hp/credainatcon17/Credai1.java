8b50a1e77906fffe52c35e36b30568e83b95bf84
Rahul <rahulsakaria97@gmail.com>
2017-02-18 19:06:22 +0530

    List item

--- app/src/main/java/com/example/hp/credainatcon17/Credai1.java
+++ app/src/main/java/com/example/hp/credainatcon17/Credai1.java
@@ -1,7 +1,5 @@
 package com.example.hp.credainatcon17;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -14,6 +12,7 @@ public class Credai1 extends Fragment {
         //required empty public constructor
 
     }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/hp/credainatcon17/Emerge.java
+++ app/src/main/java/com/example/hp/credainatcon17/Emerge.java
@@ -1,7 +1,5 @@
 package com.example.hp.credainatcon17;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,12 +7,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-
 public class Emerge extends Fragment {
 
     public Emerge() {
         //required empty for constructor
     }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
