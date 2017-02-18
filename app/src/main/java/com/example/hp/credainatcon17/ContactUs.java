15db759c50571bb7b97cf05eebec9f507f3f44ff
Rahul <rahulsakaria97@gmail.com>
2017-02-18 19:03:00 +0530

    List item

--- app/src/main/java/com/example/hp/credainatcon17/ContactUs.java
+++ app/src/main/java/com/example/hp/credainatcon17/ContactUs.java
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Window;
 import android.widget.EditText;
 
 import java.util.ArrayList;
@@ -27,7 +28,6 @@ public class ContactUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
-
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
